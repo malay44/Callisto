107392d4bed4742c66d4e4d27321b2611e51964e
malay44 <malay.p1@ahduni.edu.in>
2022-09-17 19:35:22 +0530

    google aut redirect url updated

--- config/google_passport.js
+++ config/google_passport.js
@@ -30,7 +30,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/google/redirect",
+      callbackURL: "https://callistox.herokuapp.com/google/redirect",
     },
     function (accessToken, refreshToken, profileObj, done) {
       // console.log(profileObj)
