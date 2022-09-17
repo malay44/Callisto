ffea907b3166111aaa3aeffbbb48d4a8c5373f01
malay44 <101856674+malay44@users.noreply.github.com>
2022-09-17 11:30:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,5 +87,5 @@ app.use(routes);
  * -------------- SERVER ----------------
  */
 
-// Server listens on http://localhost:3000
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port);
